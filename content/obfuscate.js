3aa90ad0131b1fd510a1c0f5251e49d31d01bf86
Michail Peterlis <pixel@localhost.localdomain>
2019-05-15 01:42:51 +0200

    removed unnecessary stuff

--- content/obfuscate.js
+++ content/obfuscate.js
@@ -18,73 +18,6 @@ function obfuscateTypedChar(charStr) {
 }
 
 
-// function getSelection(element) {
-    // var start;
-    // var end;
-    // var type;
-    //
-    // var doc = element.ownerDocument || element.document;
-    // var win = doc.defaultView || doc.parentWindow;
-    // var sel;
-    //
-    // // firefox workaround
-    // if(element.selectionStart != undefined) {
-    //     start = element.selectionStart;
-    //     end = element.selectionEnd;
-    //     type = "selectionStart";
-    // }
-    // else if (typeof win.getSelection != "undefined") {
-    //     sel = win.getSelection();
-    //     if (sel.rangeCount > 0) {
-    //         var range = win.getSelection().getRangeAt(0);
-    //         var preCaretRange = range.cloneRange();
-    //         preCaretRange.selectNodeContents(element);
-    //         preCaretRange.setEnd(range.startContainer, range.startOffset);
-    //         start = preCaretRange.toString().length;
-    //         preCaretRange.setEnd(range.endContainer, range.endOffset);
-    //         end = preCaretRange.toString().length;
-    //         type = "win.getSelection";
-    //     }
-    // }
-    // else if ( (sel = doc.selection) && sel.type != "Control") {
-    //     var textRange = sel.createRange();
-    //     var preCaretTextRange = doc.body.createTextRange();
-    //     preCaretTextRange.moveToElementText(element);
-    //     preCaretTextRange.setEndPoint("EndToStart", textRange);
-    //     start = preCaretTextRange.text.length;
-    //     preCaretTextRange.setEndPoint("EndToEnd", textRange);
-    //     end = preCaretTextRange.text.length;
-    //     type = "doc.selection";
-    // }
-    // return { start: start, end: end, type: type };
-// }
-
-
-// function replaceSelectedText(replacementText) {
-//     var sel, range;
-//     if (window.getSelection) {
-//         sel = window.getSelection();
-//         if (sel.rangeCount) {
-//             range = sel.getRangeAt(0);
-//             range.deleteContents();
-//             range.insertNode(document.createTextNode(replacementText));
-//         }
-//     } else if (document.selection && document.selection.createRange) {
-//         range = document.selection.createRange();
-//         range.text = replacementText;
-//     }
-// }
-
-
-function clearSelection() {
-    if (window.getSelection) {
-        window.getSelection().removeAllRanges();
-    } else if (document.selection) {
-        document.selection.empty();
-    }
-}
-
-
 function OnKeyPress(event) {
     event = event || window.event;
     event.preventDefault();
@@ -115,14 +48,6 @@ function OnKeyPress(event) {
                     range.collapse(true);
                     sel.removeAllRanges();
                     sel.addRange(range);
-
-                    // var range = win.getSelection().getRangeAt(0);
-                    // var preCaretRange = range.cloneRange();
-                    // preCaretRange.selectNodeContents(this);
-                    // preCaretRange.setEnd(range.startContainer, range.startOffset);
-                    // start = preCaretRange.toString().length;
-                    // preCaretRange.setEnd(range.endContainer, range.endOffset);
-                    // end = preCaretRange.toString().length;
                 }
             }
         }
