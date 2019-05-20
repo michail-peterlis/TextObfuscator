4c180009f626def5f5183306b0ed12e4f3c8730d
Peter Michailis <michapet@students.zhaw.ch>
2019-05-21 00:21:27 +0200

    cleanup and visual changes - replaced bogus url - switched from buttons to slider

--- popup/menu.js
+++ popup/menu.js
@@ -1,27 +1,9 @@
-
-$(document).ready(function() {
-    $("#LEVEL_0").click(() => {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {type:"LEVEL_0"}, function(response) {
+document.getElementById("levelslider").onchange = function() {
+    let value = Math.abs(this.value);
+    console.log(value);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type:"LEVEL_" + value}, function(response) {
 //                $("#text").text(response);
-            });
         });
     });
-
-
-
-    var activeTabId;
-
-    chrome.tabs.onActivated.addListener(function(activeInfo) {
-      activeTabId = activeInfo.tabId;
-    });
-
-
-    $("#LEVEL_1").click(() => {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {type:"LEVEL_1"}, function(response) {
-//                $("#text").text(response);
-            });
-        });
-    });
-});
+};
